227a8fabcfc61a1b7ac2f749b7cd362bc7835129
Massimo Tassinari <massimotassinariaulisi@gmail.com>
2023-07-02 10:48:00 -0400

    added

--- main.py
+++ main.py
@@ -4,58 +4,59 @@ from tkinter import PhotoImage, Label
 import grafo as g
 import funciones as f
 
+
 class Aplication(ttk.Frame):
 
     def __init__(self, main_window):
         super().__init__(main_window)
         main_window.title("MetroProject")
         main_window.geometry("700x420")
-        self.imagen = PhotoImage(file = "ruta-critica/metroproject.png")
-        main_window.background = Label(image = self.imagen, text = "fondo")
-        main_window.background.place(x = 0, y = 0, relwidth = 1, relheight = 1)
-        main_window.resizable(0,0)
-        self.icono = tk.PhotoImage(file="ruta-critica/metroproject - logo.png")
+        self.imagen = PhotoImage(file="metroproject.png")
+        main_window.background = Label(image=self.imagen, text="fondo")
+        main_window.background.place(x=0, y=0, relwidth=1, relheight=1)
+        main_window.resizable(0, 0)
+        self.icono = tk.PhotoImage(file="metroproject - logo.png")
         main_window.iconphoto(True, self.icono)
 
         self.identificacion = ttk.Entry(
-            width= 4,
+            width=4,
         )
-    
+
         self.identificacion.place(
             x=142, y=159,
         )
 
         self.duracion = ttk.Entry(
-            width= 4,
+            width=4,
         )
-    
+
         self.duracion.place(
             x=335, y=159,
         )
 
         self.predecesor = ttk.Entry(
-            width= 4,
+            width=4,
         )
-    
+
         self.predecesor.place(
             x=528, y=159,
         )
 
         self.descripcion = ttk.Entry(
-            width= 35,
+            width=35,
         )
-    
+
         self.descripcion.place(
             x=172, y=269,
         )
 
         self.anadir = tk.Button(
             main_window,
-            text = "+",
-            font = ("Inter", 20),
-            bg = "#e691ca",
-            fg = "black",
-            command= self.agregar,
+            text="+",
+            font=("Inter", 20),
+            bg="#e691ca",
+            fg="black",
+            command=self.agregar,
             relief="flat",
         )
 
@@ -63,15 +64,13 @@ class Aplication(ttk.Frame):
             x=476, y=240
         )
 
-
-
         self.ruta = tk.Button(
             main_window,
-            text = "Calcular",
-            font = ("Inter", 14),
-            bg = "#e691ca",
-            fg = "black",
-            command= self.calcular,
+            text="Calcular",
+            font=("Inter", 14),
+            bg="#e691ca",
+            fg="black",
+            command=self.calcular,
             relief="flat",
         )
 
@@ -80,6 +79,7 @@ class Aplication(ttk.Frame):
         )
 
     info = []
+
     def agregar(self):
         actividad = {}
         id = self.identificacion.get()
@@ -105,7 +105,7 @@ class Aplication(ttk.Frame):
         self.duracion.delete(0, tk.END)
         self.predecesor.delete(0, tk.END)
         self.descripcion.delete(0, tk.END)
-        
+
         print(self.info)
 
     def calcular(self):
